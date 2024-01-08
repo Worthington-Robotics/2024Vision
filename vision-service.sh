3a68cf736c8ce062451b51770835d4b7be978f34
lobsterwise <pangolin630@gmail.com>
2024-01-08 16:36:58 -0500

    2024

--- vision-service.sh
+++ vision-service.sh
@@ -1,3 +1,3 @@
-cd "/home/worbots4145/2023Vision"
+cd "/home/worbots4145/2024Vision"
 # source "/home/worbots4145/opencv_real/venv.sh"
-/home/worbots4145/2023Vision/run.sh -c "$1" -C "$2"
+/home/worbots4145/2024Vision/run.sh -c "$1" -C "$2"
--- vision-worbots.sh
+++ vision-worbots.sh
@@ -1,2 +1,2 @@
-cd /home/worbots4145/2023Vision
+cd /home/worbots4145/2024Vision
 python3 src/main.py
